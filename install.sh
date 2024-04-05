cf2ea882f7dc886074cec2185462edcdb839f355
Nitai Charan <nitaicharan@gmail.com>
2024-04-05 10:16:13 +0100

    chore(install): change installetion path

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ echo "-------------> Installing $PROGRAM"
 #$pminstall ghostscript
 
 echo "-------------> Configurando $PROGRAM"
-ln -sf $(pwd) $HOME/.config/$CONFIG_FOLDER
+ln -sf $(pwd) ~/.config/$CONFIG_FOLDER
+rm -rf ~/.local/share/tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins
